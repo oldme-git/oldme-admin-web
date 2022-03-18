aecf9fd6aee1d14898077f1048d0a403d2a96e33
wangrenyi <wangrenyi@traceint.com>
2022-03-18 18:11:21 +0800

    头像组件应用

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="app-container">
-    <el-tabs v-model="active" @tab-click="handleClick">
+    <el-tabs v-model="active">
       <el-tab-pane label="基本信息" name="info">
         <el-form ref="info" label-width="auto" :label-position="'left'" :rules="infoRules" :model="info">
           <el-row>
-            <el-col :span="8">
+            <el-col :span="10">
               <el-form-item label="昵称" prop="nickname">
                 <el-input :maxlength="100" autocomplete="off" v-model="info.nickname"/>
               </el-form-item>
@@ -19,7 +19,34 @@
           </el-form-item>
         </el-form>
       </el-tab-pane>
-      <el-tab-pane label="修改密码" name="password">用户管理</el-tab-pane>
+      <el-tab-pane label="修改密码" name="password">
+        <el-form ref="password" label-width="auto" :label-position="'left'" :rules="passwordRules" :model="password">
+          <el-row>
+            <el-col :span="10">
+              <el-form-item label="旧密码" prop="oldPwd">
+                <el-input :maxlength="100" autocomplete="off" v-model="password.oldPwd" type="password"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="10">
+              <el-form-item label="新密码" prop="pwd">
+                <el-input :maxlength="100" autocomplete="off" v-model="password.pwd" type="password"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="10">
+              <el-form-item label="再次确认" prop="newPwd">
+                <el-input :maxlength="100" autocomplete="off" v-model="password.newPwd" type="password"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-form-item label="操作">
+            <el-button type="primary" @click="submitPassword()">确认</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -35,15 +62,67 @@ export default {
     Crop
   },
   data() {
+    // 规则验证
+    const pwdRules = (rule, value, callback) => {
+      const isEdit = this.isEdit
+      if (isEdit && value === undefined) {
+        callback()
+      }
+      const pattern = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/
+      const valid = pattern.test(value)
+      if (valid) {
+        callback()
+      } else {
+        callback(new Error('至少包含数字和英文，长度6-20，不能使用特殊字符和中文字符'))
+      }
+    }
+    const reOldPwd = (rule, value, callback) => {
+      if (value === this.password.oldPwd) {
+        callback(new Error('新密码不能和旧密码一致!'));
+      } else {
+        callback();
+      }
+    }
+    const rePwd = (rule, value, callback) => {
+      if (value !== this.password.pwd) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    }
+
     return {
+      // 基本信息规则验证
       infoRules: {
         nickname: [{ required: true, trigger: 'blur', message: '昵称不能为空' }],
-        avatar: [{ required: true, trigger: 'blur', message: '请上传图片' }],
+        avatar: [{ required: true, trigger: 'blur', message: '请上传图片' }]
       },
-      active: "info",
+      // 密码规则验证
+      passwordRules: {
+        oldPwd: [
+          { required: true, trigger: 'blur', message: '请输入旧密码' },
+          { validator: pwdRules, trigger: 'blur' }
+        ],
+        pwd: [
+          { required: true, trigger: 'blur', message: '请输入新密码' },
+          { validator: pwdRules, trigger: 'blur' },
+          { validator: reOldPwd, trigger: 'blur' },
+        ],
+        newPwd: [
+          { required: true, trigger: 'blur', message: '请再次确认' },
+          { validator: pwdRules, trigger: 'blur' },
+          { validator: rePwd, trigger: 'blur' }
+        ]
+      },
+      active: 'info',
       info: {
-        nickname: "",
-        avatar: ""
+        nickname: '',
+        avatar: ''
+      },
+      password: {
+        oldPwd: '',
+        pwd: '',
+        newPwd: ''
       }
     }
   },
@@ -62,19 +141,41 @@ export default {
             const { message } = response
             this.$notify({
               title: message,
-              message: "修改成功",
+              message: '修改成功',
               duration: 5000,
               type: 'success'
             })
             // 更新信息
-            this.$store.dispatch("user/getInfo")
-          }).catch(error => {})
+            this.$store.dispatch('user/getInfo')
+          }).catch(error => {
+          })
         }
       })
     },
-    handleClick(tab, event) {
-      console.log(tab, event)
-    },
+    submitPassword() {
+      // 提交信息
+      this.$refs.info.validate((valid) => {
+        if (valid) {
+          let data = {
+            old_password: this.password.oldPwd,
+            password: this.password.pwd
+          }
+          // 发起请求
+          updatePassword(data).then(response => {
+            const { message } = response
+            this.$notify({
+              title: message,
+              message: '修改密码成功，请重新登录',
+              duration: 5000,
+              type: 'success'
+            })
+            this.$store.dispatch('user/logout')
+            this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+          }).catch(error => {
+          })
+        }
+      })
+    }
   }
 }
 </script>
